477fde2790c87eee68b8ab504ee2824a1aca02af
Cody <Cody@DESKTOP-2HTRJNL>
2016-10-18 01:31:23 +1000

    Mouse click increment counter

--- unLoot/Input.cpp
+++ unLoot/Input.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Engine.h"
+#include "Counter.h"
 
 void Engine::input() {
 
@@ -7,18 +8,17 @@ void Engine::input() {
 		m_window.close( );
 	}
 
-	if ( Mouse::isButtonPressed( Mouse::Left ) ) {
-		counter++;
-	}
-
-
 	sf::Event event;
 	while (m_window.pollEvent( event ))
 	{
 		
 		if (event.type == sf::Event::Closed) {
 			m_window.close( );
+		} else if ( event.type == event.MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left ) {
+			counter.increment;
 		}
+
+		
 			
 	}
 
